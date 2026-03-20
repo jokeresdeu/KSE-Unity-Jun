df70058c6e41a08e0b75ad56c7baa6737df2f6d9
Oles <oles.dibrivniy@ffstudios.game>
2026-03-20 07:28:32 +0200

    JU-1 Basics

--- Assets/Scripts/ChickenHunt/Props.cs
+++ Assets/Scripts/ChickenHunt/Props.cs
@@ -2,6 +2,15 @@ namespace ChickenHunt
 {
     public class Props : IDamageable
     {
-        
+        public int Hp { get; set; }
+        public void TakeDamage(int damage)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Die()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
